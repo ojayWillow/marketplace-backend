8ca5b8b57643ff7a010803ba56d35f7006100170
ojayWillow <og.vitols@gmail.com>
2026-01-01 09:13:52 +0200

    Update models __init__.py with Review and TaskResponse exports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,7 @@
 from .user import User
 from .listing import Listing
 from .task_request import TaskRequest
+from .review import Review
+from .task_response import TaskResponse
 
-__all__ = ['User', 'Listing', 'TaskRequest']
+__all__ = ['User', 'Listing', 'TaskRequest, 'Review', 'TaskResponse']
