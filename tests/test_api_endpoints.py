7bcaf2d1a0ecfc4353bc041e4c4efe5040d678ea
ojayWillow <og.vitols@gmail.com>
2026-01-02 07:25:39 +0200

    Fix test_reviews_flow to use correct API endpoint with query params

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -110,8 +110,7 @@ class TestAPIEndpoints:
         assert response.status_code == 201
         
         # Get reviews for product
-        response = client.get(f'/api/reviews/product/{product_id}')
-        assert response.status_code == 200
+        response = client.get(f'/api/reviews?listing_id={product_id}')        assert response.status_code == 200
         data = response.get_json()
         assert isinstance(data, list)
 
