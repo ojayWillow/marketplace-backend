ff16490bf35e1f5791b906bc71dd241973eabf5b
ojayWillow <og.vitols@gmail.com>
2026-01-04 20:47:53 +0200

    Add TaskApplication to models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,6 @@ from .listing import Listing
 from .task_request import TaskRequest
 from .review import Review
 from .task_response import TaskResponse
+from .task_application import TaskApplication
 
-__all__ = ['User', 'Listing', 'TaskRequest', 'Review', 'TaskResponse']
+__all__ = ['User', 'Listing', 'TaskRequest', 'Review', 'TaskResponse', 'TaskApplication']
